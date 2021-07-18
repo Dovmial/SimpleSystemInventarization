d80bde1ce334247caf4f3955d12b4b6cd853f476
Dovmial <nma91@mail.ru>
2021-07-18 20:14:34 +0500

    Add inventar nmber for equipment

--- SimpleSystemInventarization/ITequipment.hpp
+++ SimpleSystemInventarization/ITequipment.hpp
@@ -2,18 +2,34 @@
 #define _ITEQUIPMENT_HPP_
 
 #include <vector>
-#include "Components.hpp"
 #include <iostream>
+#include "Components.hpp"
 
 class ITequipment {
+public:
+	ITequipment() = delete;
+	ITequipment(int64_t inventarNumber_) :
+		inventarNumber(inventarNumber_) {}
 
+	int64_t getInventarNumber() const noexcept {
+		return inventarNumber;
+	}
+	void setInventarNumber() noexcept {
+		this->inventarNumber = inventarNumber;
+	}
+
+protected:
+	int64_t inventarNumber;
 };
 
 using Components = std::vector<BaseComponent*>;
-class PC: public ITequipment {
+
+class PC : public ITequipment {
 public:
 	PC() = delete;
-	PC(Components components_) : components(components_) {}
+	PC(Components components_, int64_t inentarNumber = -1) :
+		components(components_), ITequipment(inentarNumber)
+	{}
 protected:
 	Components components;
 };
@@ -21,7 +37,10 @@ protected:
 class Monitor : public ITequipment {
 public:
 	Monitor() = delete;
-	Monitor(float screenDiagonal_) : screenDiagonal(screenDiagonal_) {}
+	Monitor(float screenDiagonal_, int64_t inventarNumber_ = -1) :
+		screenDiagonal(screenDiagonal_),
+		ITequipment(inventarNumber_)
+	{}
 protected:
 	float screenDiagonal;
 };
@@ -29,13 +48,32 @@ protected:
 class Laptop : virtual public PC, virtual public Monitor {
 public:
 	Laptop() = delete;
-	Laptop(Components components_, float screenDiagonal_) :
-		PC(components_), Monitor(screenDiagonal_) {}
+	Laptop(Components components_, float screenDiagonal_, int64_t inventarNumber_) :
+		PC(components_, inventarNumber_),
+		Monitor(screenDiagonal_)
+	{}
 	void show() noexcept {
 		for (const auto& comp : components) {
 			std::cout << comp->getDescription() << std::endl;
 		}
 		std::cout << "Screen diagonal: " << screenDiagonal << "\"\n";
+		std::cout << "Inventar number: "<<PC::getInventarNumber() << '\n';
 	}
 };
+/*
+class Printer : public ITequipment {
+public:
+	Printer() = delete;
+
+private:
+
+};
+class Scanner : public ITequipment {
+
+};
+
+class MFU : virtual public Printer, virtual public Scanner {
+
+};
+*/
 #endif // !_ITEQUIPMENT_HPP_
--- SimpleSystemInventarization/main.cpp
+++ SimpleSystemInventarization/main.cpp
@@ -16,7 +16,7 @@ int main() {
 	CPU cpuLaptop(2.4f, "2.4 GHz");
 	RAM ramLaptop(16.f, "16 GB");
 	Components compLaptop = { &cpuLaptop, &ramLaptop };
-	Application SSI(Laptop(compLaptop, 19));
+	Application SSI(Laptop(compLaptop, 19, 4101340041));
 	SSI.start();
 
 	return 0;
