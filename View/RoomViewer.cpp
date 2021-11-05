e611d27aeab864600e014b929dbb1ee221d51131
Dovmial <nma91@mail.ru>
2021-11-06 01:23:49 +0500

    add QStandardItemModel for tables

--- View/RoomViewer.cpp
+++ View/RoomViewer.cpp
@@ -6,18 +6,43 @@ RoomViewer::RoomViewer(std::unique_ptr<DataManager> dm, QWidget* parent)
     dataManager{ std::move(dm) },
     room{ nullptr },
     currentLocationInfo(new QLabel(this)),
+    deviceTableModel(new QStandardItemModel(this)),
+    serviceTableModel(new QStandardItemModel(this)),
+    problemSolutionTableModel(new QStandardItemModel(this)),
     QMainWindow(parent)
 {
     ui->setupUi(this);
     room = dataManager->getCurrentRoom();
     ui->lblRoomName->setText(QString::fromStdString(room->getName()));
-    QString status = QString::fromStdString(
-        "Build: " + dataManager->getCurrentLocationInfo().first +
-        "\tRoom: " + dataManager->getCurrentLocationInfo().second
-    );
-    currentLocationInfo->setText(status);
+    
     currentLocationInfo->setStyleSheet("QLabel {color: blue;}");
+    setTextStatusBar(
+        dataManager->getCurrentLocationInfo().first,
+        dataManager->getCurrentLocationInfo().second);
     ui->statusbar->addWidget(currentLocationInfo);
+
+    deviceTableModel->setColumnCount(2);
+    deviceTableModel->setHorizontalHeaderLabels(
+        QStringList()
+        << QStringLiteral(u"Оборудование") << QStringLiteral(u"Инвентарный номер")
+    );
+    ui->tvItems->setModel(deviceTableModel);
+
+    serviceTableModel->setColumnCount(2);
+    serviceTableModel->setHorizontalHeaderLabels(
+        QStringList()
+        << QStringLiteral(u"Обслуживание") << QStringLiteral(u"Дата")
+    );
+    ui->tvServiceItems->setModel(serviceTableModel);
+
+    problemSolutionTableModel->setColumnCount(3);
+    problemSolutionTableModel->setHorizontalHeaderLabels(
+        QStringList()
+        << QStringLiteral(u"Проблема")
+        << QStringLiteral(u"Решение")
+        << QStringLiteral(u"Дата")
+    );
+    ui->tvProblemSolutionItems->setModel(problemSolutionTableModel);
 }
 
 RoomViewer::~RoomViewer()
@@ -31,6 +56,12 @@ void RoomViewer::setRoom(Room* room_) {
 Room* RoomViewer::getRoom()const {
     return room;
 }
+void RoomViewer::setTextStatusBar(const std::string& str1, const std::string& str2)
+{
+    QString status = QString::fromStdString(
+        "Build: " + str1 + "\tRoom: " + str2);
+    currentLocationInfo->setText(status);
+}
 void RoomViewer::on_mnuAddItem_triggered() {
     auto pCrItemDialog{ new CreateItemDialog(nullptr) };
     if (pCrItemDialog->exec() == CreateItemDialog::Accepted) {
@@ -38,7 +69,8 @@ void RoomViewer::on_mnuAddItem_triggered() {
         if (dialog->exec() == QDialog::Accepted) {
             room->addItem(dialog->createItem());
             ui->pteInfoItems->setPlainText(
-                QString::fromStdString(room->showItem(0)->getITequipment()->getInfo()));    
+                QString::fromStdString(room->showItem(0)->getITequipment()->getInfo())
+            );    
         }
 
     }
--- View/RoomViewer.hpp
+++ View/RoomViewer.hpp
@@ -5,21 +5,29 @@
 #include "../SimpleSystemInventarization/DataManager.hpp"
 #include "../SimpleSystemInventarization/Room.hpp"
 #include <memory>
+#include <QStandardItemModel>
 
 class RoomViewer : public QMainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
 public:
-    RoomViewer(std::unique_ptr<DataManager> dm, QWidget* parent = Q_NULLPTR);
-    ~RoomViewer();
+	RoomViewer(std::unique_ptr<DataManager> dm, QWidget* parent = Q_NULLPTR);
+	~RoomViewer();
+public:
+	void setRoom(Room* room_);
+	Room* getRoom()const;
 
-    void setRoom(Room* room_);
-    Room* getRoom()const;
+
 private slots:
-    void on_mnuAddItem_triggered();
+	void on_mnuAddItem_triggered();
 private:
-    Ui::RoomViewer* ui;
-    std::unique_ptr<DataManager> dataManager;
-    Room* room;
-    QLabel* currentLocationInfo;
+	void setTextStatusBar(const std::string& str1, const std::string& str2);
+private:
+	Ui::RoomViewer* ui;
+	std::unique_ptr<DataManager> dataManager;
+	Room* room;
+	QLabel* currentLocationInfo;
+	QStandardItemModel* deviceTableModel;
+	QStandardItemModel* serviceTableModel;
+	QStandardItemModel* problemSolutionTableModel;
 };
